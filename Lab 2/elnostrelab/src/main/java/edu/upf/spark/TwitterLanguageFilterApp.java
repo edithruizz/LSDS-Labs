60396fcdd5f5e4898e09d6624aff7a4260d4738a
edithruizz <edith.ruiz01@estudiant.upf.edu>
2023-02-27 15:49:09 +0100

    uf

--- Lab 2/elnostrelab/src/main/java/edu/upf/spark/TwitterLanguageFilterApp.java
+++ Lab 2/elnostrelab/src/main/java/edu/upf/spark/TwitterLanguageFilterApp.java
@@ -6,26 +6,31 @@ import edu.upf.model.SimplifiedTweet;
 
 public class TwitterLanguageFilterApp {
 	public static void main( String[] args ) throws Exception {
-	String language = args[0];
+	  String language = args[0];
     String output = args[1];
     String input = args[2];
-    System.out.println("Language: " + language + ". Output file: " + output + ". input file/folder: " + input);
+    
+    System.out.println("Language: " + language + ". Output: " + output + ". Input: " + input);
 
-
-  //Create a SparkContext to initialize
-    SparkConf conf = new SparkConf().setAppName("Filter App");
+    // Create a SparkContext to initialize
+    SparkConf conf = new SparkConf().setAppName("TwitterLanguageFilterApp");
     JavaSparkContext sparkContext = new JavaSparkContext(conf);
+    
     // Load input
     JavaRDD<String> lines = sparkContext.textFile(input);
 
-    JavaRDD<SimplifiedTweet> stweet = lines
+    JavaRDD<SimplifiedTweet> tweets = lines
         .map(t -> SimplifiedTweet.fromJson(t))
-        .filter(opt-> opt.isPresent())
-        .map(g ->g.get())
-        .filter(fst->language.equals(fst.getLanguage()));
+        .filter(o -> o.isPresent())
+        .map(g -> g.get())
+        .filter(f -> language.equals(f.getLanguage()));
     
-
-    stweet.saveAsTextFile(output);
-
+    System.out.println("Total tweets: " + tweets.count());
+    tweets.saveAsTextFile(output);
 	}
+
+  private static String normalise(String word) {
+    return word.trim().toLowerCase();
+  }
+
 }
